4e421688304d8a369fd9fcf7d338fd6f97cac5c9
giirrr <ikl1121@naver.com>
2023-09-13 05:37:58 +0900

    일단 쌩 하드코딩으로 db박스 전까지 다함

---
+++ src/components/Dbbar.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import React, { useRef, useState } from "react";
+import "../App.css";
+import Navbar from "../pages/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+const Dbbar = ({ children }) => {
+  return (
+    <div>
+      <div className="navbar-container">
+        <Navbar />
+        <div className="content">
+          <h1 className="maintitle">전체</h1>
+          <Nav className="유형바" variant="underline" defaultActiveKey="/home">
+            <Nav.Item>
+              <Nav.Link eventKey="link-1">All</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-2">OD</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-3">AD</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-4">CP</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-5">CD</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-6">JD</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-7">H</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-8">X</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-9">Y</Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <hr
+            style={{ marginTop: "-2px", marginLeft: "24px", height: "15px" }}
+          />
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dbbar;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,13 +2,27 @@ import axios from "axios";
 import React, { useRef, useState } from "react";
 import "../App.css";
 import Navbar from "./Navbar";
+import Nav from "react-bootstrap/Nav";
+import Dbbar from "../components/Dbbar";
 
 const Main = () => {
   return (
     <div>
-      <Navbar />
-      <span>응애</span>
-      웅애
+      <div className="Db_container">
+        <Dbbar>
+          <div className="Add_Btn">
+            <button>Add</button>
+          </div>
+          <div className="Db_content">
+            <div>DB 분배일</div>
+            <div>고객유형</div>
+            <div>이름</div>
+            <div>생년월일 (나이)</div>
+            <div>연락처</div>
+            <div>거주지</div>
+          </div>
+        </Dbbar>
+      </div>
     </div>
   );
 };
