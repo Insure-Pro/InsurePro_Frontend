3d631d2e7f6c0d22c86d3be88ec9d69cacc5961b
giirrr <ikl1121@naver.com>
2023-09-17 08:01:26 +0900

    드롭다운버튼 디자인 바꿔야함 일단 실패

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,9 +3,21 @@ import React, { useRef, useState, useEffect } from "react";
 import "../App.css";
 import Dbbar from "../components/Dbbar";
 import Modal1 from "../components/Modal";
+import { Dropdown, ButtonGroup, Button } from "react-bootstrap";
+import DropdownButton from "react-bootstrap/DropdownButton";
 
 const Main = () => {
   const [customers, setCustomers] = useState([]); // 상태를 추가하여 고객 데이터를 저장합니다.
+  const dropdownButtonStyles = {
+    backgroundColor: "#e0e0e0", // 연한 회색
+    color: "black", // 글자색
+    borderColor: "#b0b0b0", // 테두리 색
+  };
+
+  const dropdownItemStyles = {
+    fontSize: "16px", // 2px 더 큰 글자 크기
+    padding: "3px 3px", // 박스 크기 조절
+  };
 
   useEffect(() => {
     // 데이터를 로드하는 함수를 정의합니다.
@@ -35,8 +47,33 @@ const Main = () => {
     <div>
       <div className="Db_container">
         <Dbbar>
-          <div className="Add_Btn">
-            <Modal1 />
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <div className="Add_Btn">
+              <Modal1 />
+            </div>
+            <DropdownButton
+              id="dropdown-basic-button"
+              size="sm"
+              title="정렬기준"
+              variant="secondary"
+              style={dropdownButtonStyles}
+            >
+              <Dropdown.Item href="#/action-1" style={dropdownItemStyles}>
+                최신순
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-2" style={dropdownItemStyles}>
+                지역별
+              </Dropdown.Item>
+              <Dropdown.Item href="#/action-3" style={dropdownItemStyles}>
+                나이대별
+              </Dropdown.Item>
+            </DropdownButton>
           </div>
           <div className="Db_content">
             <div>DB 분배일</div>
