8af42208c1e10a3714284b31a9d642c6cb406101
giirrr <ikl1121@naver.com>
2023-09-19 03:24:50 +0900

    DB창 일단 디자인 성공><

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,6 +5,7 @@ import Dbbar from "../components/Dbbar";
 import Modal1 from "../components/Modal";
 import { Dropdown, ButtonGroup, Button } from "react-bootstrap";
 import DropdownButton from "react-bootstrap/DropdownButton";
+import ListGroup from "react-bootstrap/ListGroup";
 
 const Main = () => {
   const [customers, setCustomers] = useState([]); // 상태를 추가하여 고객 데이터를 저장합니다.
@@ -17,6 +18,157 @@ const Main = () => {
   const dropdownItemStyles = {
     fontSize: "16px", // 2px 더 큰 글자 크기
     padding: "3px 3px", // 박스 크기 조절
+  };
+  //DB분배일
+  const listItemStyle1 = {
+    flex: "none", // flex 속성 제거
+    width: "270px",
+    textAlign: "center",
+    padding: "13px", // 아이템 내부 여백 조절
+    marginBottom: "8px",
+    marginLeft: "8px",
+    borderRightRadius: "20px",
+    borderRadius: "5px",
+    // borderRadius: "0 5px 5px 0",//오른쪽 모서리
+  };
+  //고객유형
+  const listItemStyle2 = {
+    flex: "none", // flex 속성 제거
+    width: "90px",
+    textAlign: "center",
+    padding: "13px", // 아이템 내부 여백 조절
+    marginBottom: "8px",
+    marginLeft: "8px",
+    borderLeft: "1px solid #ddd",
+    borderRadius: "5px",
+  };
+  //이름
+  const listItemStyle3 = {
+    flex: "none", // flex 속성 제거
+    width: "70px",
+    textAlign: "center",
+    padding: "13px", // 아이템 내부 여백 조절
+    marginBottom: "8px",
+    marginLeft: "8px",
+    borderLeft: "1px solid #ddd",
+    borderRadius: "5px",
+  };
+  //생년월일(나이)
+  const listItemStyle4 = {
+    flex: "none", // flex 속성 제거
+    width: "200px",
+    textAlign: "center",
+    padding: "13px", // 아이템 내부 여백 조절
+    marginBottom: "8px",
+    marginLeft: "8px",
+    borderLeft: "1px solid #ddd",
+    borderRadius: "5px",
+  };
+  //연락처
+  const listItemStyle5 = {
+    flex: "none", // flex 속성 제거
+    width: "150px",
+    textAlign: "center",
+    padding: "13px", // 아이템 내부 여백 조절
+    marginBottom: "8px",
+    marginLeft: "8px",
+    borderLeft: "1px solid #ddd",
+    borderRadius: "5px",
+  };
+  //거주지
+  const listItemStyle6 = {
+    flex: "none", // flex 속성 제거
+    width: "250px",
+    textAlign: "center",
+    padding: "13px", // 아이템 내부 여백 조절
+    marginBottom: "8px",
+    marginLeft: "8px",
+    borderLeft: "1px solid #ddd",
+    borderRadius: "5px",
+    // borderRadius: "5px 0 0 5px",// 왼쪽모서리
+  };
+  const listItemTitleStyle1 = {
+    flex: "none", // flex 속성 제거
+    width: "270px",
+    textAlign: "center",
+    padding: "13px", // 아이템 내부 여백 조절
+    marginBottom: "8px",
+    marginLeft: "8px",
+    borderRightRadius: "20px",
+    borderRadius: "5px",
+    border: "none",
+    color: "#FFF",
+    backgroundColor: "#175CD3",
+    opacity: "0.9",
+  };
+  const listItemTitleStyle2 = {
+    flex: "none", // flex 속성 제거
+    width: "90px",
+    textAlign: "center",
+    padding: "13px", // 아이템 내부 여백 조절
+    marginBottom: "8px",
+    marginLeft: "8px",
+    borderLeft: "1px solid #ddd",
+    borderRadius: "5px",
+    color: "#FFF",
+    backgroundColor: "#175CD3",
+    opacity: "0.9",
+  };
+  const listItemTitleStyle3 = {
+    flex: "none", // flex 속성 제거
+    width: "70px",
+    textAlign: "center",
+    padding: "13px", // 아이템 내부 여백 조절
+    marginBottom: "8px",
+    marginLeft: "8px",
+    borderLeft: "1px solid #ddd",
+    borderRadius: "5px",
+    border: "none",
+    color: "#FFF",
+    backgroundColor: "#175CD3",
+    opacity: "0.9",
+  };
+  const listItemTitleStyle4 = {
+    flex: "none", // flex 속성 제거
+    width: "200px",
+    textAlign: "center",
+    padding: "13px", // 아이템 내부 여백 조절
+    marginBottom: "8px",
+    marginLeft: "8px",
+    borderLeft: "1px solid #ddd",
+    borderRadius: "5px",
+    border: "none",
+    color: "#FFF",
+    backgroundColor: "#175CD3",
+    opacity: "0.9",
+  };
+  const listItemTitleStyle5 = {
+    flex: "none", // flex 속성 제거
+    width: "150px",
+    textAlign: "center",
+    padding: "13px", // 아이템 내부 여백 조절
+    marginBottom: "8px",
+    marginLeft: "8px",
+    borderLeft: "1px solid #ddd",
+    borderRadius: "5px",
+    border: "none",
+    color: "#FFF",
+    backgroundColor: "#175CD3",
+    opacity: "0.9",
+  };
+  const listItemTitleStyle6 = {
+    flex: "none", // flex 속성 제거
+    width: "250px",
+    textAlign: "center",
+    padding: "13px", // 아이템 내부 여백 조절
+    marginBottom: "8px",
+    marginLeft: "8px",
+    borderLeft: "1px solid #ddd",
+    borderRadius: "5px",
+    border: "none",
+    color: "#FFF",
+    backgroundColor: "#175CD3",
+    opacity: "0.9",
   };
 
   useEffect(() => {
@@ -75,31 +227,56 @@ const Main = () => {
               </Dropdown.Item>
             </DropdownButton>
           </div>
-          <div className="Db_content">
-            <div>DB 분배일</div>
-            <div>고객유형</div>
-            <div>이름</div>
-            <div>생년월일 (나이)</div>
-            <div>연락처</div>
-            <div>거주지</div>
-            <>
-              {/* 가져온 데이터를 화면에 출력합니다. */}
-              {customers
-                .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)) // 최신 순으로 정렬합니다.
-                .map((customer) => (
-                  <React.Fragment key={customer.pk}>
-                    <div>{customer.createdAt}</div>
-                    <div>{customer.customerTypeString}</div>
-                    <div>{customer.name}</div>
-                    <div>
-                      {customer.birth} 만 ({customer.age})세
-                    </div>
-                    <div>{customer.phone}</div>
-                    <div>{customer.address}</div>
-                  </React.Fragment>
-                ))}
-            </>
-          </div>
+          <ListGroup
+            horizontal
+            style={{ marginTop: "12px", marginBottom: "-10px" }}
+          >
+            <ListGroup.Item variant="primary" style={listItemTitleStyle1}>
+              DB 분배일
+            </ListGroup.Item>
+            <ListGroup.Item variant="primary" style={listItemTitleStyle2}>
+              고객유형
+            </ListGroup.Item>
+            <ListGroup.Item variant="primary" style={listItemTitleStyle3}>
+              이름
+            </ListGroup.Item>
+            <ListGroup.Item variant="primary" style={listItemTitleStyle4}>
+              생년월일 (나이)
+            </ListGroup.Item>
+            <ListGroup.Item variant="primary" style={listItemTitleStyle5}>
+              연락처
+            </ListGroup.Item>
+            <ListGroup.Item variant="primary" style={listItemTitleStyle6}>
+              거주지
+            </ListGroup.Item>
+          </ListGroup>
+          <hr />
+          {/* 가져온 데이터를 화면에 출력합니다. */}
+          {customers
+            .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)) // 최신 순으로 정렬합니다.
+            .map((customer) => (
+              <ListGroup horizontal key={customer.pk}>
+                <ListGroup.Item style={listItemStyle1}>
+                  {customer.registerDate}
+                </ListGroup.Item>
+                <ListGroup.Item style={listItemStyle2}>
+                  {customer.customerTypeString}
+                </ListGroup.Item>
+                <ListGroup.Item style={listItemStyle3}>
+                  {customer.name}
+                </ListGroup.Item>
+                <ListGroup.Item style={listItemStyle4}>
+                  {customer.birth} 만 ({customer.age})세
+                </ListGroup.Item>
+                <ListGroup.Item style={listItemStyle5}>
+                  {customer.phone}
+                </ListGroup.Item>
+                <ListGroup.Item style={listItemStyle6}>
+                  {customer.address}
+                </ListGroup.Item>
+              </ListGroup>
+            ))}
+          ;
         </Dbbar>
       </div>
     </div>
