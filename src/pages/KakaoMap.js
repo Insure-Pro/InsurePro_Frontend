eeea9231df64dd60af2c4eec7dc6694e28c19ecc
giirrr <ikl1121@naver.com>
2023-11-24 21:30:43 +0900

    Feat: 현재위치를 기본위치값으로 설정

--- src/pages/KakaoMap.js
+++ src/pages/KakaoMap.js
@@ -5,45 +5,74 @@ import axios from "axios";
 
 const KakaoMap = () => {
   const MAIN_URL = process.env.REACT_APP_MAIN_URL;
+  const [mapCenter, setMapCenter] = useState({
+    lat: 35.1379222, // Default latitude (Busan City Hall)
+    lng: 129.05562775, // Default longitude (Busan City Hall)
+  });
+  const [locationObtained, setLocationObtained] = useState(false);
+
+  const initializeMap = () => {
+    const container = document.getElementById("map");
+    const options = {
+      center: new window.kakao.maps.LatLng(mapCenter.lat, mapCenter.lng),
+      level: 3,
+    };
+    const map = new window.kakao.maps.Map(container, options);
+    const geocoder = new window.kakao.maps.services.Geocoder();
+
+    axios
+      .get(`${MAIN_URL}/customers/latest`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+      .then((response) => {
+        response.data.forEach((customer) => {
+          geocoder.addressSearch(customer.address, function (result, status) {
+            if (status === window.kakao.maps.services.Status.OK) {
+              const coords = new window.kakao.maps.LatLng(
+                result[0].y,
+                result[0].x
+              );
+              new window.kakao.maps.Marker({
+                map: map,
+                position: coords,
+              });
+            }
+          });
+        });
+      })
+      .catch((error) => console.log(error));
+  };
 
   useEffect(() => {
-    const initializeMap = () => {
-      const container = document.getElementById("map");
-      const options = {
-        center: new window.kakao.maps.LatLng(35.1379222, 129.05562775),
-        level: 3,
-      };
-
-      const map = new window.kakao.maps.Map(container, options);
-      const geocoder = new window.kakao.maps.services.Geocoder();
-
-      axios
-        .get(`${MAIN_URL}/customers/latest`, {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-          },
-        })
-        .then((response) => {
-          response.data.forEach((customer) => {
-            geocoder.addressSearch(customer.address, function (result, status) {
-              if (status === window.kakao.maps.services.Status.OK) {
-                const coords = new window.kakao.maps.LatLng(
-                  result[0].y,
-                  result[0].x
-                );
-                new window.kakao.maps.Marker({
-                  map: map,
-                  position: coords,
-                });
-              }
-            });
+    if (!locationObtained && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setMapCenter({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
           });
-        })
-        .catch((error) => console.log(error));
-    };
+          setLocationObtained(true);
+        },
+        () => {
+          console.log("Unable to retrieve your location");
+          setLocationObtained(true);
+        },
+        {
+          enableHighAccuracy: false,
+          maximumAge: 0,
+          timeout: Infinity,
+        }
+      );
+    }
+  }, [locationObtained]);
 
-    window.kakao.maps.load(initializeMap);
-  }, []);
+  useEffect(() => {
+    if (locationObtained) {
+      window.kakao.maps.load(initializeMap);
+    }
+  }, [locationObtained, mapCenter]);
 
   return (
     <div
