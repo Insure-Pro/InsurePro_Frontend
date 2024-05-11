82ac709a8f22455c75630dd877fcfca90c8feeb5
giirrr <ikl1121@naver.com>
2024-05-11 17:09:28 +0900

    Style: 모바일웹 고객 카드 스켈레톤UI,애니메이션 추가, delay 1초 추가

---
+++ src/Skeleton/SkeletonMain.js
@@ -0,0 +1,40 @@
+import React from "react";
+import SkeletonElement from "./SkeletonElement";
+
+const SkeletonMain = () => {
+  const mobileMenuIcon = process.env.PUBLIC_URL + "/mobileMenuIcon.png";
+  return (
+    <div className="skeleton-wrapper  flex h-[132px] w-[162px] flex-col rounded bg-Secondary-50/20  py-3 pl-4 pr-2 ">
+      <div class="mb-2 flex justify-between">
+        <div class="flex">
+          <SkeletonElement type="customertype" />
+
+          <SkeletonElement type="current" />
+        </div>
+        <div class=" h-5 w-5">
+          <img src={mobileMenuIcon} />
+        </div>
+      </div>
+      <div class="mb-1 flex items-center">
+        {/* <div class="mr-0.5"> {customer.name}</div>{" "} */}
+        <SkeletonElement type="name" />
+        {/* <div class="mr-1.5"> ({customer.age})</div> */}
+        <SkeletonElement type="age" />
+        {/* <img class="h-[14px] w-[14px]" src={check_off} /> */}
+        <SkeletonElement type="circle" />
+      </div>
+
+      <div class="mb-2 ">
+        <SkeletonElement type="phone" />
+      </div>
+      <div class="mb-2  ">
+        <SkeletonElement type="registerDate" />
+      </div>
+      <div class="">
+        <SkeletonElement type="address" />
+      </div>
+    </div>
+  );
+};
+
+export default SkeletonMain;
--- src/components/Main/MobileCustomerList.js
+++ src/components/Main/MobileCustomerList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import { useCustomerTypes } from "../../hooks/CustomerTypes/useCustomerTypes";
+import SkeletonMain from "../../Skeleton/SkeletonMain";
 
 function MobileCustomerList({
   customers,
@@ -49,81 +50,106 @@ function MobileCustomerList({
   }
   return (
     <>
-      <div class=" flex w-full justify-center">
-        <div
-          class={`sm:[380px] mx-4 grid h-full gap-1 xsm:grid-cols-2 xsm:gap-1 sm:grid-cols-3 sm:gap-2 md:grid-cols-4 md:gap-3 `}
-        >
-          {customers
-            .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-            .map((customer) => (
-              <div key={customer.pk} data-id={customer.pk} className="  ">
-                <ListGroup
-                  key={customer.pk}
-                  onClick={() => handleCustomerClick(customer)}
-                >
-                  <div class="">
-                    <div
-                      className={`customerCardWrapper h-[122px] w-[162px] rounded border ${
-                        customer.contractYn
-                          ? " border-Primary-400"
-                          : " border-black/50"
-                      } bg-white py-3 pl-4 pr-2`}
-                    >
-                      <div class="mb-2 flex justify-between">
-                        <div class="flex">
-                          <button
-                            style={{
-                              backgroundColor: getColorByTypeName(
-                                customer.customerType.name,
-                              ),
-                            }}
-                            key={customer.pk}
-                            class="mr-1 flex h-4 w-6 items-center justify-center rounded"
-                          >
-                            <div class="text-[10px] font-normal text-white">
-                              {customer.customerType.name}
-                            </div>
-                          </button>
-                          <button class="flex h-4 w-[44px] items-center justify-center rounded border border-Success-300 bg-Success-50">
-                            <span class="text-[10px] font-normal text-Success-300">
-                              상담현황
-                            </span>
-                          </button>
-                        </div>
-                        <div
-                          class=" h-5 w-5 cursor-pointer"
-                          onClick={(e) => handleMobileContextMenu(e, customer)}
-                        >
-                          <img src={mobileMenuIcon} />
-                        </div>
-                      </div>
-                      <div class="mb-1 flex items-center text-sm text-LightMode-Text">
-                        <div class="mr-0.5"> {customer.name}</div>{" "}
-                        <div class="mr-1.5"> ({customer.age})</div>
-                        {customer.contractYn ? (
-                          <img class=" h-[14px] w-[14px] " src={check_on} />
-                        ) : (
-                          <img class="h-[14px] w-[14px]" src={check_off} />
-                        )}
-                      </div>
-
-                      <div class="mb-2 flex text-xs text-LightMode-Subtext">
-                        {customer.phone}
-                      </div>
-                      <div class="mb-2 flex text-[10px] font-normal text-Secondary-300">
-                        분배일 : {customer.registerDate}{" "}
-                      </div>
-                      <div class="flex text-[10px] font-normal text-Secondary-300">
-                        주소 : {customer.dongString}
-                        {customer.address}{" "}
-                      </div>
-                    </div>
-                  </div>
-                </ListGroup>
-              </div>
-            ))}
+      {isLoading ? (
+        <div class="flex w-full justify-center">
+          <div
+            class={`sm:[380px] mx-4 grid h-full gap-1 xsm:grid-cols-2 xsm:gap-1 sm:grid-cols-3 sm:gap-2 md:grid-cols-4 md:gap-3 `}
+          >
+            <SkeletonMain />
+            <SkeletonMain />
+            <SkeletonMain />
+            <SkeletonMain />
+            <SkeletonMain />
+            <SkeletonMain />
+            <SkeletonMain />
+            <SkeletonMain />
+            <SkeletonMain />
+            <SkeletonMain />
+            <SkeletonMain />
+            <SkeletonMain />
+          </div>
         </div>
-      </div>
+      ) : (
+        <>
+          <div class=" flex w-full justify-center">
+            <div
+              class={`sm:[380px] mx-4 grid h-full gap-1 xsm:grid-cols-2 xsm:gap-1 sm:grid-cols-3 sm:gap-2 md:grid-cols-4 md:gap-3 `}
+            >
+              {customers
+                .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+                .map((customer) => (
+                  <div key={customer.pk} data-id={customer.pk} className="  ">
+                    <ListGroup
+                      key={customer.pk}
+                      onClick={() => handleCustomerClick(customer)}
+                    >
+                      <div class="">
+                        <div
+                          className={`customerCardWrapper h-[122px] w-[162px] rounded border ${
+                            customer.contractYn
+                              ? " border-Primary-400"
+                              : " border-black/50"
+                          } bg-white py-3 pl-4 pr-2`}
+                        >
+                          <div class="mb-2 flex justify-between">
+                            <div class="flex">
+                              <button
+                                style={{
+                                  backgroundColor: getColorByTypeName(
+                                    customer.customerType.name,
+                                  ),
+                                }}
+                                key={customer.pk}
+                                class="mr-1 flex h-4 w-6 items-center justify-center rounded"
+                              >
+                                <div class="text-[10px] font-normal text-white">
+                                  {customer.customerType.name}
+                                </div>
+                              </button>
+                              <button class="flex h-4 w-[44px] items-center justify-center rounded border border-Success-300 bg-Success-50">
+                                <span class="text-[10px] font-normal text-Success-300">
+                                  상담현황
+                                </span>
+                              </button>
+                            </div>
+                            <div
+                              class=" h-5 w-5 cursor-pointer"
+                              onClick={(e) =>
+                                handleMobileContextMenu(e, customer)
+                              }
+                            >
+                              <img src={mobileMenuIcon} />
+                            </div>
+                          </div>
+                          <div class="mb-1 flex items-center text-sm text-LightMode-Text">
+                            <div class="mr-0.5"> {customer.name}</div>{" "}
+                            <div class="mr-1.5"> ({customer.age})</div>
+                            {customer.contractYn ? (
+                              <img class=" h-[14px] w-[14px] " src={check_on} />
+                            ) : (
+                              <img class="h-[14px] w-[14px]" src={check_off} />
+                            )}
+                          </div>
+
+                          <div class="mb-2 flex text-xs text-LightMode-Subtext">
+                            {customer.phone}
+                          </div>
+                          <div class="mb-2 flex text-[10px] font-normal text-Secondary-300">
+                            분배일 : {customer.registerDate}{" "}
+                          </div>
+                          <div class="flex text-[10px] font-normal text-Secondary-300">
+                            주소 : {customer.dongString}
+                            {customer.address}{" "}
+                          </div>
+                        </div>
+                      </div>
+                    </ListGroup>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
