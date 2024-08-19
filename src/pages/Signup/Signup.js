efce64fdd4b1f6b4e7b1bca95623bde75bfe7d62
giirrr <ikl1121@naver.com>
2024-08-19 21:38:34 +0900

    Chore: 회원가입 팀 기본값 수정

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -126,7 +126,7 @@ const Signup = () => {
         companyPk = 3;
         break;
       default:
-        console.error("회사를 선택해주세요.");
+        // console.error("회사를 선택해주세요.");
         return; // 팀이 선택되지 않았다면 함수 실행을 중단
     }
 
