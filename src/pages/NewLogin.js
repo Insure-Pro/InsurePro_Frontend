c4a7fd6cad7c6fe6f8687edf4e79ae4b63796908
giirrr <ikl1121@naver.com>
2024-08-28 09:00:14 +0900

    Test: 로그인 UI 테스트3

--- src/pages/NewLogin.js
+++ src/pages/NewLogin.js
@@ -6,16 +6,16 @@ const NewLogin = ({ onStart }) => {
       {/* <Navbar /> */}
       <div className="login_mobile_img absolute   flex justify-center">
         <div class="relative flex flex-col items-center">
-          <div class="relative top-[60px] h-[64px] w-[290px] text-[24px] font-semibold leading-8 text-white">
+          <div class="relative top-[40px] h-[64px] w-[290px] text-[24px] font-semibold leading-8 text-white">
             고객 관리는 우리가 해줄게요. 영업에만 집중하세요.
           </div>
           <div class="flex justify-center">
-            <div class="relative top-[70px] mt-3 h-[44px] w-[180px]  text-[14px] font-medium leading-8 text-[#EBF1FF]">
+            <div class="relative top-[50px] mt-3 h-[44px] w-[180px]  text-[14px] font-medium leading-8 text-[#EBF1FF]">
               INSUREPRO는 보험 설계사의 고객 관리를 서포팅해줍니다.
             </div>
           </div>
           <div
-            class="relative top-[500px] flex h-[42px] w-[330px] cursor-pointer items-center justify-center rounded bg-white text-base font-semibold text-Primary-300 shadow-[0_0_16px_0_rgba(0,0,0,0.25)] hover:bg-Primary-300 hover:text-white"
+            class="relative top-[460px] flex h-[42px] w-[330px] cursor-pointer items-center justify-center rounded bg-white text-base font-semibold text-Primary-300 shadow-[0_0_16px_0_rgba(0,0,0,0.25)] hover:bg-Primary-300 hover:text-white"
             onClick={onStart}
           >
             INSUREPRO 이용하기
